196337312548a484c7dbadb7d3a8a256507e71e2
Devinml <devinlink8@gmail.com>
2020-08-13 14:19:36 -0600

    added merge of acc data

--- src/concate_data.py
+++ src/concate_data.py
@@ -6,18 +6,22 @@ directory = '/home/devin/Documents/Galvanize/repos/Human-activity-recognition-us
 out_arr = np.empty([0,3])
 
 filepath = directory+'/labels.txt'
-df = pd.read_csv(filepath, header=None, delim_whitespace=True)
+out_df = pd.DataFrame(columns=['X', 'Y', 'Z','label'])
+label_df = pd.read_csv(filepath, header=None, delim_whitespace=True)
 exp_num = 1
 for entry in sorted(os.listdir(directory)):
     if (entry != 'labels.txt')and ('gyro' not in entry):
         dir_ = directory+ '/'+ entry
         b = pd.read_csv(dir_,header=None,delim_whitespace=True)
+        b.columns = ['X', 'Y', 'Z']
+        b['label'] = 0
         empty = np.zeros(b.shape)
-        for label in np.nditer(labels):
-            pass
-            # labels[]
-        
-        out_arr = np.concatenate((out_arr,b))
-print(out_arr.shape)
+        for label in label_df.iterrows():
+            if label[1][0] == int(entry[7:9]):
+                b.loc[label[1][3]-2: label[1][4]-2, 'label'] = label[1][2]
+        out_df = pd.concat([out_df,b])
+
+out_df.to_csv('/home/devin/Documents/Galvanize/repos/Human-activity-recognition-using-smartphone-data/data/merged_data.csv')
+
 
 
