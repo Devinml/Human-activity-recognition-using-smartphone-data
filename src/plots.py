30c961c9ca8da6b0f69285d66e938c306f3a93ff
Devinml <devinlink8@gmail.com>
2020-08-26 13:29:51 -0600

    added plot images

--- src/plots.py
+++ src/plots.py
@@ -27,7 +27,10 @@ def acc_at_activity(df, acc_metric):
 
 def boxplots(df, fig, ax, acc_metric):
     data = acc_at_activity(df, acc_metric)
-    box = plt.boxplot(data, showfliers=False, patch_artist=True)
+    box = plt.boxplot(data,
+                      showfliers=False,
+                      patch_artist=True,
+                      medianprops=dict(color='white'))
     colors = ['blue', 'red', 'green', 'black', 'violet', 'orange']
     for patch, color in zip(box['boxes'], colors):
         patch.set_facecolor(color)
@@ -37,26 +40,28 @@ def boxplots(df, fig, ax, acc_metric):
                                           'Sittting',
                                           'Standing',
                                           'Laying'])
-    ax.set_title(acc_metric + ' acc at each activity')
+    ax.set_title(acc_metric + ' acc at Activity')
     ax.set_ylabel('Acc(gs)')
     ax.set_xlabel('Activity')
     plt.xticks(rotation=45)
 
 
 def main():
+    font = {'size'   : 16}
+    plt.rc('font', **font)
     df = pd.read_csv('data/merged_data_save.csv')
     plt.style.use('ggplot')
-    fig, ax = plt.subplots(figsize=(12, 8))
+    fig, ax = plt.subplots(figsize=(10, 9))
     boxplots(df=df, fig=fig, ax=ax, acc_metric='X')
-    fig2, ax2 = plt.subplots(figsize=(12, 8))
+    fig2, ax2 = plt.subplots(figsize=(10, 9))
     boxplots(df=df, fig=fig2, ax=ax2, acc_metric='Y')
-    fig3, ax3 = plt.subplots(figsize=(12, 8))
+    fig3, ax3 = plt.subplots(figsize=(10, 9))
     boxplots(df=df, fig=fig3, ax=ax3, acc_metric='Z')
-    fig4, ax4 = plt.subplots(figsize=(12, 8))
+    fig4, ax4 = plt.subplots(figsize=(10, 9))
     boxplots(df=df, fig=fig4, ax=ax4, acc_metric='gyroX')
-    fig5, ax5 = plt.subplots(figsize=(12, 8))
+    fig5, ax5 = plt.subplots(figsize=(10,9))
     boxplots(df=df, fig=fig5, ax=ax5, acc_metric='gyroY')
-    fig6, ax6 = plt.subplots(figsize=(12, 8))
+    fig6, ax6 = plt.subplots(figsize=(10, 9))
     boxplots(df=df, fig=fig6, ax=ax6, acc_metric='gyroZ')
     plt.show()
 
